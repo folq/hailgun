7b14f83f273e657202084ded1df68c234c1f87f4
Robert Massaioli <rmassaioli@atlassian.com>
2014-07-08 08:47:58 +1000

    Validate email adresses.

--- Mail/Hailgun.hs
+++ Mail/Hailgun.hs
@@ -1,5 +1,8 @@
 module Mail.Hailgun where
 
+import Data.ByteString
+import Text.Email.Validate
+
 {- 
  - The basic rest API's look like this when used in curl:
  -
@@ -14,15 +17,22 @@ module Mail.Hailgun where
  - This is what we need to emulate with this library.
  -}
 
-type EmailAddress = String
+type UnverifiedEmailAddress = ByteString
 type MessageSubject = String
-data MessageBody 
-   = TextBody String
-   | HTMLBody String
+data MessageContent
+   = TextOnly 
+      { textContent :: String
+      }
+   | TextAndHTML
+      { textContent :: String
+      , htmlContent :: String
+      }
+
+
 
 data HailgunMessage = HailgunMessage
    { messageSubject  :: MessageSubject
-   , messageBody     :: MessageBody
+   , messageContent  :: MessageContent
    , messageFrom     :: EmailAddress
    , messageTo       :: [EmailAddress]
    , messageCC       :: [EmailAddress]
@@ -40,3 +50,29 @@ data HailgunMessage = HailgunMessage
    -- TODO o:tracking-opens support
    -- TODO custom mime header support
    -- TODO custome message data support
+
+data MessageRecipients = MessageRepipients 
+   { recipientsTo    :: [UnverifiedEmailAddress]
+   , recipientsCC    :: [UnverifiedEmailAddress]
+   , recipientsBCC   :: [UnverifiedEmailAddress]
+   }
+
+type HailgunErrorMessage = String
+
+hailgunMessage :: MessageSubject -> MessageContent -> UnverifiedEmailAddress -> MessageRecipients -> Either HailgunErrorMessage HailgunMessage
+hailgunMessage subject content sender recipients = do
+   from <- validate sender
+   to <- mapM validate (recipientsTo recipients)
+   cc <- mapM validate (recipientsCC recipients)
+   bcc <- mapM validate (recipientsBCC recipients)
+   return HailgunMessage 
+      { messageSubject = subject
+      , messageContent = content
+      , messageFrom = from
+      , messageTo = to
+      , messageCC = cc
+      , messageBCC = bcc
+      }
+
+-- Use this method of the HTTP library to convert this into a Request body:
+-- https://hackage.haskell.org/package/HTTP-4000.2.17/docs/Network-HTTP-Base.html#v:urlEncodeVars
