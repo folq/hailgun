be1664e291a8c6d92093d90af9be8c35625f0ab9
Robert Massaioli <rmassaioli@atlassian.com>
2014-07-11 16:35:15 +1000

    Working on the API to send emails.
    The send email API is working better all of the time.

--- Mail/Hailgun.hs
+++ Mail/Hailgun.hs
@@ -1,7 +1,18 @@
-module Mail.Hailgun where
+module Mail.Hailgun 
+   ( HailgunMessage
+   , MessageSubject
+   , MessageContent(..)
+   , MessageRecipients(..)
+   ) where
 
-import Data.ByteString
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (mzero)
+import Data.Aeson
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.Text as T
 import Text.Email.Validate
+import Network.HTTP (simpleHTTP, postRequestWithBody, urlEncodeVars, rspCode)
 
 {- 
  - The basic rest API's look like this when used in curl:
@@ -17,18 +28,16 @@ import Text.Email.Validate
  - This is what we need to emulate with this library.
  -}
 
-type UnverifiedEmailAddress = ByteString
+type UnverifiedEmailAddress = B.ByteString
 type MessageSubject = String
 data MessageContent
    = TextOnly 
-      { textContent :: String
+      { textContent :: B.ByteString
       }
    | TextAndHTML
-      { textContent :: String
-      , htmlContent :: String
+      { textContent :: B.ByteString
+      , htmlContent :: B.ByteString
       }
-
-
 
 data HailgunMessage = HailgunMessage
    { messageSubject  :: MessageSubject
@@ -74,5 +83,62 @@ hailgunMessage subject content sender recipients = do
       , messageBCC = bcc
       }
 
+toPostVars :: HailgunMessage -> [(String, String)]
+toPostVars message = 
+   [ ("from", show . messageFrom $ message)
+   , ("subject", messageSubject message)
+   ] ++ to 
+   ++ cc 
+   ++ bcc 
+   ++ fromContent (messageContent message)
+   where
+      to = convertEmails "to" . messageTo $ message
+      cc = convertEmails "cc" . messageCC $ message
+      bcc = convertEmails "bcc" . messageBCC $ message
+
+      fromContent :: MessageContent -> [(String, String)]
+      fromContent t@(TextOnly _) = [ ("text", BC.unpack $ textContent t) ]
+      fromContent th@(TextAndHTML {}) = ("html", BC.unpack $ htmlContent th) : fromContent (TextOnly . textContent $ th)
+
+      convertEmails :: String -> [EmailAddress] -> [(String, String)]
+      convertEmails prefix = fmap ((,) prefix . show)
+
 -- Use this method of the HTTP library to convert this into a Request body:
 -- https://hackage.haskell.org/package/HTTP-4000.2.17/docs/Network-HTTP-Base.html#v:urlEncodeVars
+
+data HailgunContext = HailgunContext
+   { hailgunDomain :: String -- TODO better way to represent a domain
+   , hailgunApiKey :: String
+   }
+
+data HailgunSendResponse = HailgunSendResponse
+   { hsrMessage :: String
+   , hsrId      :: String
+   }
+
+instance FromJSON HailgunSendResponse where
+   parseJSON (Object v) = HailgunSendResponse
+      <$> v .: T.pack "message"
+      <*> v .: T.pack "id"
+   parseJSON _ = mzero
+
+sendEmail :: HailgunContext -> HailgunMessage -> IO (Either HailgunErrorMessage HailgunSendResponse)
+sendEmail context message = do
+   result <- simpleHTTP (postRequestWithBody url contentType body)
+   case result of
+      Left connectionError -> error "The connection failed"
+      Right response -> case rspCode response of
+         (2, 0, 0) -> error "Request successful but no response gathered"
+         (4, 0, 0) -> error "Bad Request - Often missing a required parameter"
+         (4, 0, 1) -> error "Unauthorized - No valid API key provided"
+         (4, 0, 2) -> error "Request Failed - Parameters were valid but request failed"
+         (4, 0, 4) -> error "Not Found - The requested item doesn’t exist"
+         (5, 0, x) -> if x `elem` (0 : [2..4]) 
+                        then error "Server Errors - something is wrong on Mailgun’s end"
+                        else error "NonStandard Error"
+         (x, y, z) -> error "NonStandard Error"
+   where
+      url = "https://api.mailgun.net/v2/" ++ hailgunDomain context ++ "/messages"
+      contentType = undefined
+      body = urlEncodeVars . toPostVars $ message
+
---
+++ README.markdown
@@ -0,0 +1,10 @@
+# Hailgun
+
+The hailgun library is a Haskell library that integrates with Mailgun. Mailgun is a service that
+allows controlled sending of emails. The intial focus of the API is on sending emails rather than 
+recieving them.
+
+This library was implimented based upon the information provided on the [Mailgun API Reference][1].
+The goal of this library is match this API 1-1.
+
+ [1]: http://documentation.mailgun.com/api_reference.html
